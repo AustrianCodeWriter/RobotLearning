73c245c71d0ad62542bae46ee13ba4fbe58411d4
AustrianCodeWriter <53706346+AustrianCodeWriter@users.noreply.github.com>
2024-01-14 18:50:50 +0100

    Update install_packages.bat

--- install/install_packages.bat
+++ install/install_packages.bat
@@ -1,12 +1,31 @@
 @echo off
 
-set packagelist=packages.txt
+set PyPath=C:\Python_3.10\python.exe
 
-set packagelist=%~dp0%packagelist%
-set packagelist=%packagelist:\=/%
+echo:
+echo Installing numpy ^(1^.24^.3^)^:
+"%PyPath%" -m pip install numpy==1.24.3
 
+echo:
+echo Installing gymnasium ^(0^.28^.1^)^:
+"%PyPath%" -m pip install gymnasium==0.28.1
 
-"C:\Python_3.10\python.exe" -m pip install -r "%packagelist%"
+echo:
+echo Installing mujoco ^(2^.3^.7^)^:
+"%PyPath%" -m pip install mujoco==2.3.7
+
+echo:
+echo Installing wandb ^(0^.15^.0^)^:
+"%PyPath%" -m pip install wandb==0.15.0
+
+echo:
+echo Installing imageio^:
+"%PyPath%" -m pip install imageio
+
+echo:
+echo Installing torch ^(2^.1^.2^) with CUDA support^:
+"%PyPath%" -m pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
+
 
 echo|set /p="Press any key to exit..."
 pause >nul 2>&1
